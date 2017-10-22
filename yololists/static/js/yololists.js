afcb07b9ecfc573ee14a3a68f3aac4cc40e0dece
Alexandre Aubin <alexandre.aubin@cern.ch>
2017-10-22 03:38:08 +0200

    Looks like subscribe is working now ;)

--- yololists/static/js/yololists.js
+++ yololists/static/js/yololists.js
@@ -1,4 +1,9 @@
+
+// When loading the subscribe modal
 $('#modal-subscribe').on('show.bs.modal', function (event) {
+
+    $("#modal-subscribe-answer").text("")
+
 	// Button that triggered the modal
     var button = $(event.relatedTarget)
 
@@ -10,4 +15,43 @@ $('#modal-subscribe').on('show.bs.modal', function (event) {
     var modal = $(this)
     modal.find('.modal-subscribe-list-name').text(list_name)
     modal.find('.modal-subscribe-list-display-name').text(list_display_name)
-})
+    modal.find("[name=list-name]").val(list_name)
+});
+
+
+// When submitting the subscribe modal
+$('#modal-subscribe-form').on('submit', function(e){
+	
+    e.preventDefault();
+    
+    // Set some nice 'loading' effect on the submit button...
+    var $submit_button = $("#modal-subscribe-submit");
+    $submit_button.button("loading");
+    setTimeout(function() {
+        $submit_button.button('reset');
+    }, 1000);
+
+    // Submit the form
+	$.ajax({
+		url: "/subscribe",
+		type: 'POST',
+		data: $('#modal-subscribe-form').serialize(),
+		success: function(data) {
+            $submit_button.button('reset');
+            // Display the answer
+            if (data.status == "OK")
+            {
+                $("#modal-subscribe-answer").addClass("text-success");
+                $("#modal-subscribe-answer").removeClass("text-danger");
+            }
+            else
+            {
+                $("#modal-subscribe-answer").addClass("text-danger");
+                $("#modal-subscribe-answer").removeClass("text-success");
+            }
+            $("#modal-subscribe-answer").text(data.message);
+		}
+	});
+});
+                            
+
--- yololists/yololists.py
+++ yololists/yololists.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from mailman import Mailman
 
 app = Flask(__name__)
@@ -10,3 +10,42 @@ def index():
     data = { "lists": Mailman().lists() }
 
     return render_template("index.html", **data)
+
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+
+    # Get the FORM data
+    required_fields = [ "list-name", "email" ]
+    if [ f for field in required_fields if field not in request.form ]:
+        return jsonify({ "status": "NOTOK", "message": "Missing form data :/" })
+
+    if request.form["antispam"] != "":
+        return jsonify({ "status": "NOTOK", "message": "Sneaky spammer :|" })
+
+    list_name = request.form["list-name"]
+    email = request.form["email"]
+
+    # Attempt to subscribe
+    try:
+        Mailman().subscribe(list_name, email)
+    # Return messages depending on outcome
+    except Exception as e_:
+        e = str(e_)
+        if e == "UnknownList":
+            message = "Uh oh, the list you are attempting to subscribe to doesn't exist?"
+        elif e == "SelfSubscribe":
+            message = "Nice try, but you can't subscribe the list to itself ;)"
+        elif e == "BadEmail":
+            message = "Sorry, this does not look like a valid email address :/"
+        elif e == "HostileEmail":
+            message = "Sorry, this looks like an hostile email address and cannot be subscribed :/"
+        elif e == "MemberBanned":
+            message = "This email has been banned from subscribing to this list."
+        else:
+            message = "An unknown error happened. Sorry about that :(" + str(e)
+
+        return jsonify({ "status": "NOTOK", "message": message})
+    else:
+        message = "Cool! Please check your inbox to proceed with the confirmation ;)."
+        return jsonify({ "status": "OK", "message": message })
